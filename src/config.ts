c8beb4493871c839d346ac19cd0f99f2d9f9e0c3
HongDat <33374303+HongTanDat1810@users.noreply.github.com>
2025-11-21 13:33:08 +0700

    Update config.ts
    Signed-off-by: HongDat <33374303+HongTanDat1810@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycb.../exec"; // dán URL web app vào đây
+  "https://script.google.com/macros/s/AKfycbwDiBTomppsfBWbHtFYKjzlOIpPMueblwxwQGeCR7xAe4Kpy1h74md7OhATS6iP8WTmaA/exec"; // dán URL web app vào đây
